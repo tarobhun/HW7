a60c9138f1c6526d376116c4fb7cb94c3401e546
Micah Coldiron <mcc4339@rit.edu>
2024-02-15 17:33:35 -0800

    individual recipe pages created and debugged.

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ $(document).ready(function(){
         recipes = JSON.parse(localStorage.getItem("recipes"));
         recipeIdCounter = JSON.parse(localStorage.getItem("counter"));
     });
-    if (window.location.href === "recipes.html") {
+    if (window.location.href == "file:///C:/Users/micah/IntrotoC%23/HW7/recipes.html") {
+        recipes = JSON.parse(localStorage.getItem("recipes"));
+        recipeIdCounter = JSON.parse(localStorage.getItem("counter"));
         if (recipes.length > 0) {
             // Clear existing content in the textbox
             $("#recipeList").empty();

f077cbedb36249155418e59584193f70859fe909
tarobhun <hunheidi@ymail.com>
2024-02-20 19:46:53 -0800

    console

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
         $("#add").css("visibility", "hidden");
         $("#recipe-details").css("visibility", "hidden");
         $("#recipes").css("visibility", "visible");
+        displayRecipes(); // Display recipes when the #recipes element becomes visible
     });
     
     $(".newr").click(function() {
@@ -23,34 +24,92 @@ $(document).ready(function(){
         $("#home").css("visibility", "visible");
     });
 
-    if ($("#recipes").is(":visible") && recipes.length > 0) {
-        // Clear existing content in the textbox
-        $("#recipeList").empty();
-        // Display each recipe
-        for(let i = 0; i < recipes.length; i++) {
-            let current = recipes[i];
-            $("#recipeList").append("<li class='recipeItem' data-id='" 
-                + current.id 
-                + "' data-name='" 
-                + current.name 
-                + "' data-author='" 
-                + current.author 
-                + "' data-date='" 
-                + current.date 
-                + "' data-cuisine='" 
-                + current.cuisine 
-                + "' data-meal='" 
-                + current.meal 
-                + "' data-recipe='" 
-                + current.recipe 
-                + "'>" 
-                + current.name 
-                + "</li>");
-            $("#recipeList").append("<hr>");
+    $("#buttonAdd").click(function(){
+        let current = {
+            id: recipeIdCounter,
+            name: $("#name").val(),
+            author: $("#author").val(),
+            date: $("#date").val(),
+            cuisine: $("#select-cuisine").val(),
+            meal: $("#select-meal").val(),
+            recipe: $("#recipe").val()
+        };
+        recipes.push(current);
+        recipeIdCounter++;
+        localStorage.setItem("recipes", JSON.stringify(recipes));
+        localStorage.setItem("counter", JSON.stringify(recipeIdCounter));
+
+        // Display the new recipe in the recipe list
+        // $("#recipeList").append("<li class='recipeItem' data-id='" 
+        //     + current.id 
+        //     + "' data-name='" 
+        //     + current.name 
+        //     + "' data-author='" 
+        //     + current.author 
+        //     + "' data-date='" 
+        //     + current.date 
+        //     + "' data-cuisine='" 
+        //     + current.cuisine 
+        //     + "' data-meal='" 
+        //     + current.meal 
+        //     + "' data-recipe='" 
+        //     + current.recipe
+        //     + "'>" 
+        //     + current.name 
+        //     + "</li>");
+        // $("#recipeList").append("<hr>");
+
+        alert("Recipe added successfully. View the Recipes page.");
+
+        // Reset form fields
+        $("#name").val("");
+        $("#author").val("");
+        $("#date").val("");
+        $("#select-cuisine").val("");
+        $("#select-meal").val("");
+        $("#recipe").val("");
+
+        // Show the home section after adding the recipe
+        $("#recipe-details").css("visibility", "hidden");
+        $("#add").css("visibility", "hidden");
+        $("#recipes").css("visibility", "hidden");
+        $("#home").css("visibility", "visible");
+    });  
+
+    // Function to display recipes
+    function displayRecipes() {
+        if (recipes.length > 0) {
+            // Clear existing content in the textbox
+            $("#recipeList").empty();
+            // Display each recipe
+            for(let i = 0; i < recipes.length; i++) {
+                let current = recipes[i];
+                $("#recipeList").append("<li class='recipeItem' data-id='" 
+                    + current.id 
+                    + "' data-name='" 
+                    + current.name 
+                    + "' data-author='" 
+                    + current.author 
+                    + "' data-date='" 
+                    + current.date 
+                    + "' data-cuisine='" 
+                    + current.cuisine 
+                    + "' data-meal='" 
+                    + current.meal 
+                    + "' data-recipe='" 
+                    + current.recipe 
+                    + "'>" 
+                    + current.name 
+                    + "</li>");
+                $("#recipeList").append("<hr>");
+            }
+        } else {
+            alert("Please add a recipe");
         }
-    } else if ($("#recipes").is(":visible") && recipes.length === 0) {
-        alert("Please add a recipe");
     }
+
+    // Call the displayRecipes function when the page loads initially
+    displayRecipes();
 
     // Get the parent element (ul) of recipe items
     const recipeList = document.getElementById("recipeList");
@@ -67,6 +126,14 @@ $(document).ready(function(){
             let cuisine = event.target.dataset.cuisine;
             let meal = event.target.dataset.meal;
             let recipe = event.target.dataset.recipe;
+
+            console.log("ID:", id);
+            console.log("Name:", name);
+            console.log("Author:", author);
+            console.log("Date:", date);
+            console.log("Cuisine:", cuisine);
+            console.log("Meal:", meal);
+            console.log("Recipe:", recipe);
 
             // Construct an item to use for loading the details page
             let details = {
@@ -91,56 +158,4 @@ $(document).ready(function(){
             $("#recipe").text(details.Recipe);
         }
     });
-
-    $("#buttonAdd").click(function(){
-        let current = {
-            id: recipeIdCounter,
-            name: $("#name").val(),
-            author: $("#author").val(),
-            date: $("#date").val(),
-            cuisine: $("#select-cuisine").val(),
-            meal: $("#select-meal").val(),
-            recipe: $("#recipe").val()
-        };
-        recipes.push(current);
-        recipeIdCounter++;
-        localStorage.setItem("recipes", JSON.stringify(recipes));
-        localStorage.setItem("counter", JSON.stringify(recipeIdCounter));
-
-    // Display the new recipe in the recipe list
-    $("#recipeList").append("<li class='recipeItem' data-id='" 
-        + current.id 
-        + "' data-name='" 
-        + current.name 
-        + "' data-author='" 
-        + current.author 
-        + "' data-date='" 
-        + current.date 
-        + "' data-cuisine='" 
-        + current.cuisine 
-        + "' data-meal='" 
-        + current.meal 
-        + "' data-recipe='" 
-        + current.recipe
-        + "'>" 
-        + current.name 
-        + "</li>");
-    $("#recipeList").append("<hr>");
-
-        alert("Recipe added successfully. View the Recipes page.");
-
-        // Reset form fields
-        $("#name").val("");
-        $("#author").val("");
-        $("#date").val("");
-        $("#select-cuisine").val("");
-        $("#select-meal").val("");
-        $("#recipe").val("");
-
-        // Show the home section after adding the recipe
-        $("#recipe-details").css("visibility", "hidden");
-        $("#add").css("visibility", "hidden");
-        $("#recipes").css("visibility", "hidden");
-        $("#home").css("visibility", "visible");
-    });  
 });
